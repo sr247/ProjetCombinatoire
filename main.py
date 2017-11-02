50367da8ed6988b4b5cb22a1ef678488fd010748
tmarti2 <twenty2bar@gmail.com>
2017-11-02 10:52:48 +0100

    Main

---
+++ main.py
@@ -0,0 +1,11 @@
+# coding: utf-8
+from rules import AbstractRule
+from rules import ConstructorRule
+from rules import ConstantRule
+from rules import EpsilonRule
+from rules import SingletonRule
+from rules import ProductRule
+from rules import UnionRule
+
+if __name__ == '__main__':
+    print(4)
