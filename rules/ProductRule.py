ae2c8fe36865f62af7cce7c694813213b191cd4d
tmarti2 <twenty2bar@gmail.com>
2017-11-03 16:00:01 +0100

    Product toujours...

--- rules/ProductRule.py
+++ rules/ProductRule.py
@@ -6,6 +6,8 @@ class ProductRule(ConstructorRule):
     def __init__(self, fst, snd, cons):
         super().__init__((fst,snd))
         self._constructor = cons
+        self.cAcB = []
+
 
     def _calc_valuation(self):
         valGauche = self._grammar[self._parameters[0]].valuation()
@@ -14,12 +16,14 @@ class ProductRule(ConstructorRule):
 
     def count(self,i):
         res = 0
+        self.cAcB = []
         for k in range(i+1):
             l = i-k
             if k >= self._grammar[self._parameters[0]].valuation():
                 if l >= self._grammar[self._parameters[1]].valuation():
                     cG = self._grammar[self._parameters[0]].count(k)  
                     cD = self._grammar[self._parameters[1]].count(l)  
+                    self.cAcB.append(cG*cD)
                     res += cG*cD
         return res
 
@@ -36,10 +40,12 @@ class ProductRule(ConstructorRule):
                             res.append(self._constructor((g,d)))
         return res
 
-    def unrank(self,n,r):
+    def unrank(self,n,r):        
         c = self.count(n)
         if r >= c:
             raise ValueError("Le rang r (%d) doit etre strictement inférieur au nombre d'objets de taille %d (%d)"%(r,i,c))
+
+        
         
         
 
